3f74094e69ba8c31ffdc78aca5fdfb945ec3308f
Khalid Hazazi <hazazik1@udayton.edu>
2023-11-11 19:48:14 -0500

    Enable password change and reset

--- Bookmarks/bookmarks/account/urls.py
+++ Bookmarks/bookmarks/account/urls.py
@@ -7,7 +7,14 @@ urlpatterns = [
   #path('login/', views.user_login, name='login'),
   path('login/', auth_views.LoginView.as_view(), name='login'),
   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+  # change password urls
   path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
   path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+  # reset password urls
+  path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+  path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+  path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+  path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+  # dashboard url
   path('', views.dashboard, name='dashboard'),
 ]
